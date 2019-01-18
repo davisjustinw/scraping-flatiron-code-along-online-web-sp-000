397a46f238a81e8e731844380a8bb9efbba3ac40
Justin Davis <davisjustinw@gmail.com>
2019-01-18 18:40:04 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,14 +1,14 @@
 class Course
   attr_accessor :title, :schedule, :description
   
-  @@all
+  @@all = []
   
   def self.all 
     @@all
   end 
   
   def self.reset_all
-    all.clear  
+    @@all.clear  
   end
 end
 
