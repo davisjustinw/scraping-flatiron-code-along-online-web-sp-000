032ed02fd37405556831f9b26613574ae5dd92fb
Justin Davis <davisjustinw@gmail.com>
2019-01-18 18:44:54 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,11 +1,13 @@
 require 'nokogiri'
 require 'open-uri'
-
+require 'pry'
 require_relative './course.rb'
 
 class Scraper
   
-  
+  def get_page
+    
+  end 
   def print_courses
     self.make_courses
     Course.all.each do |course|
